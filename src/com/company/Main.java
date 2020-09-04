0b46a0a96eac7056a46c5801c384754d4c0a7196
Najse <34438787+Najse@users.noreply.github.com>
2020-09-04 21:08:27 +0200

    9/4/2020

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,16 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
-        new Boom().show();
+        var a = new ArrayList<Integer>(List.of(1, 2, 4, 5, 6, 7, 7, 8));
+
+        var r = a.stream()
+                .reduce(Integer::sum);
+
+        System.out.println(r.get());
     }
 
 }
--- src/com/company/PoppingBlocks.java
+++ src/com/company/PoppingBlocks.java
@@ -1,46 +1,46 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class PoppingBlocks extends Challenge {
 
-    private static void popOff(char[] charArray) {
-        var popArray = new ArrayList<Character>();
-        var arr = new ArrayList<Character>();
+    /*
+    When two blocks of the same "type" are adjacent to each other, the entire contiguous block
+    disappears (pops off). If this occurs, this can allow previously separated blocks to be in
+    contact with each other, setting off a chain reaction
+     */
+    private char[] popOff(char[] charArray) {
+        StringBuilder sb = new StringBuilder(new String(charArray));
 
-        for (var i : charArray) {
-            popArray.add(i);
+        int index = getRepeatingIndexOfChar(sb.toString());
+        while (index != -1) {
+            int indexOfLastRepeatingChar = index;
+
+            while (indexOfLastRepeatingChar < sb.length() && sb.charAt(indexOfLastRepeatingChar) == sb.charAt(index))
+                indexOfLastRepeatingChar++;
+
+            sb.delete(index, indexOfLastRepeatingChar);
+            index = getRepeatingIndexOfChar(sb.toString());
         }
+        return sb.toString().toCharArray();
 
-        for (int i = 0, j = 1; i < popArray.size(); i++, j++) {
-            char curr = popArray.get(i);
-            char next = i != popArray.size() - 1 ? popArray.get(j) : popArray.get(i);
 
-            if (Character.valueOf(next) == Character.valueOf(curr)) {
-                arr.add(curr);
-                arr.add(next);
-                curr = next;
-                next = j != popArray.size() - 1 ? popArray.get(j + 1) : popArray.get(j);
-                i++;
-                j++;
-                if (j != popArray.size() - 1 && Character.valueOf(curr) == Character.valueOf(next)) {
-                    arr.add(next);
-                }
-            }
-        }
-        arr.forEach(System.out::println);
     }
 
-    private static boolean moreThanTwo(char curr, char next) {
-        return false;
+    private int getRepeatingIndexOfChar(String s) {
+        for (var i = 0; i < s.length() - 1; i++) {
+            char curr = s.charAt(i);
+            char next = s.charAt(i + 1);
+            if (curr == next)
+                return i;
+        }
+        return -1;
     }
 
 
     @Override
     public void show() {
-        popOff(new char[]{'A', 'B', 'A', 'A', 'A', 'B', 'B'});
+        System.out.println(popOff(new char[]{'A', 'B', 'A', 'A', 'A', 'B', 'B', 'C', 'B', 'C',
+                'C'}));
     }
+
 }
